322a18c6dd1e0a79cdab995475c3f359d5715100
孙建华 <cumtsjh@163.com>
2017-05-03 17:39:18 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,36 @@
+JF图片服务 PHP SDK
+
+## 说明
+http 请求使用 Guzzle 三方库（v5.3），具体文档可参考：https://github.com/guzzle/guzzle
+
+## 安装
+```
+composer require jufeng/phpsdk
+```
+
+## 使用方法
+```
+require 'vendor/autoload.php';
+
+// 实例化客户端
+$client = new JF\FileManager('ak', 'sk', 'http://img-upload.20hn.cn/v1');
+// 设置上传策略，无特殊需求可省略。参考：参考：http://git.20hn.cn/developer/img-server/wikis/api-des
+// $client->setPolicy(['deadline' => time() + 3600, 'autoCompress' => 1]);
+// 获取token
+$token = $client->getToken();
+
+// 文件上传
+try {
+    $response = $client->upload($filePath = '/tmp/test.jpeg', $token);
+    // http 请求返回内容
+    echo $response->getBody();
+    // http 响应状态码
+    echo $response->getStatusCode();
+} catch (GuzzleHttp\Exception\BadResponseException $e) {
+    // 4xx 或 5xx 错误
+    $response = $e->getResponse();
+    echo $response->getStatusCode();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+```
---
+++ src/FileManager.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace JF;
+
+use GuzzleHttp\Client;
+
+class FileManager
+{
+    /**
+     * http client
+     *
+     * @var GuzzleHttp\Client
+     */
+    protected $client;
+    protected $ak;
+    protected $sk;
+    /**
+     * 上传策略。
+     *
+     * @var array
+     */
+    protected $policy;
+
+    public function __construct($ak, $sk, $baseUrl = 'http://img-upload.20hn.cn/v1')
+    {
+        $this->ak = $ak;
+        $this->sk = $sk;
+        $this->client = new Client(['base_url' => rtrim($baseUrl, '/')]);
+        $this->policy = [
+            'deadline' => time() + 3600,
+            'autoCompress' => 1,
+        ];
+    }
+
+    /**
+     * 设置上传策略
+     *
+     * @param array $policy 上传策略
+     * @return void
+     */
+    public function setPolicy(array $policy)
+    {
+        $this->policy = $policy;
+    }
+
+    /**
+     * 获取token
+     *
+     * @return string token
+     */
+    public function getToken()
+    {
+        $encodeStr = base64_encode(json_encode($this->policy));
+        return base64_encode($this->ak) . ':' . $encodeStr . ':' .
+            base64_encode(hash_hmac('sha256', $encodeStr, $this->sk));
+    }
+
+    /**
+     * 文件上传
+     *
+     * @param string $file 文件路径
+     * @return
+     */
+    public function upload($file, $token)
+    {
+        if (!is_readable($file)) {
+            throw new \InvalidArgumentException("文件不存在或无读取权限：" . $file);
+        }
+
+        return $this->client->post('/upload', [
+                'body' => [
+                    'token' => $token,
+                    'file' => fopen($file, 'r'),
+                ]
+            ]);
+    }
+}
