d2ef1fb3a2147704c85b87c667b49b0d47d299b0
孙建华 <cumtsjh@163.com>
2017-06-01 16:59:38 +0800

    change class name

--- tests/FileManagerTest.php
+++ tests/FileManagerTest.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/FileManager.php';
 
-class FileManager extends PHPUnit_Framework_TestCase
+class FileManagerTest extends PHPUnit_Framework_TestCase
 {
     protected $client;
 
